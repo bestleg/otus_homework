a80c130c8b3c145b82c3521a5a84c2a75973c923
Oleg Losyakov <olosyakov@ozon.ru>
2021-09-15 18:02:07 +0300

    good job

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,20 +8,34 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+const unicodeZero = 0x30
+const emptyString = ""
+
 func Unpack(inputStr string) (string, error) {
 
-	if inputStr == "" {
-		return inputStr, nil
+	if inputStr == emptyString {
+		return emptyString, nil
 	}
 
 	runes := []rune(inputStr)
 	lenRunes := len(runes) - 1
 	var resultString strings.Builder
 
+	if unicode.IsDigit(runes[0]) {
+		return emptyString, ErrInvalidString
+	}
+
 	for i := 0; i < len(runes[:lenRunes]); i++ {
 		if unicode.IsDigit(runes[i+1]) {
-			resultString.WriteString(strings.Repeat(string(runes[i]), int(runes[i+1])-48))
+			resultString.WriteString(strings.Repeat(string(runes[i]), int(runes[i+1])-unicodeZero))
 			i++
+			if i == lenRunes { //last rune is digit
+				break
+			}
+			err := check2Digits(runes[i+1])
+			if err != nil {
+				return emptyString, err
+			}
 		} else {
 			resultString.WriteRune(runes[i])
 		}
@@ -32,4 +46,11 @@ func Unpack(inputStr string) (string, error) {
 	}
 
 	return resultString.String(), nil
+}
+
+func check2Digits(a rune) error {
+	if unicode.IsDigit(a) {
+		return ErrInvalidString
+	}
+	return nil
 }

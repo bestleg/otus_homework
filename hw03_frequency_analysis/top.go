917c9d7a3f1f67b2a843018a3885ce9099f3d5b3
Oleg Losyakov <olosyakov@ozon.ru>
2021-09-17 17:08:02 +0300

    test commit

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -12,9 +12,9 @@ func Top10(text string) []string {
 	//tempStr2 := strings.Split(tempStr," ")
 	tempStr2 := strings.Fields(text)
 	//sort.Strings(tempStr2)
-	sort.Slice(tempStr2, func(i, j int) bool {
-		return tempStr2[i] < tempStr2[j]
-	})
+	//sort.Slice(tempStr2, func(i, j int) bool {
+	//	return tempStr2[i] < tempStr2[j]
+	//})
 	//for _, word := range tempStr2 {
 	//re := regexp.MustCompile(`[[:blank:]]|[[:space:]]`)
 	//word = re.ReplaceAllString(word,"")
@@ -28,6 +28,32 @@ func Top10(text string) []string {
 			m[word] = m[word] + 1
 		}
 	}
-	fmt.Println(m["Кристофер"])
+	keys := make([]int, 0, len(m))
+	for _, v := range m {
+		keys = append(keys, v)
+	}
+	sort.Ints(keys)
+	RemoveDuplicates(&keys)
+	for _, chislo := range keys {
+		for k, v := range m {
+			if v == chislo {
+				fmt.Println(k, m[k])
+			}
+		}
+	}
+
 	return nil
+}
+
+func RemoveDuplicates(xs *[]int) {
+	found := make(map[int]bool)
+	j := 0
+	for i, x := range *xs {
+		if !found[x] {
+			found[x] = true
+			(*xs)[j] = (*xs)[i]
+			j++
+		}
+	}
+	*xs = (*xs)[:j]
 }

5f6dfa127aa3edea6e115692eaff59e46b7fe1d4
Oleg Losyakov <olosyakov@ozon.ru>
2021-09-17 16:48:57 +0300

    initial commit

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,33 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	//tempStr := strings.Replace(text,"\t"," ",-1)
+	//strings.Fields(tempStr," ")
+	//tempStr2 := strings.Split(tempStr," ")
+	tempStr2 := strings.Fields(text)
+	//sort.Strings(tempStr2)
+	sort.Slice(tempStr2, func(i, j int) bool {
+		return tempStr2[i] < tempStr2[j]
+	})
+	//for _, word := range tempStr2 {
+	//re := regexp.MustCompile(`[[:blank:]]|[[:space:]]`)
+	//word = re.ReplaceAllString(word,"")
+	//fmt.Println(word)
+	//}
+	m := make(map[string]int)
+	for _, word := range tempStr2 {
+		if _, ok := m[word]; !ok {
+			m[word] = 1
+		} else {
+			m[word] = m[word] + 1
+		}
+	}
+	fmt.Println(m["Кристофер"])
 	return nil
 }

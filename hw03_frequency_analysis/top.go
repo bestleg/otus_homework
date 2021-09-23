438349e8e5722766a051d388f3033149ec5af6ef
Oleg Losyakov <olosyakov@ozon.ru>
2021-09-23 21:05:53 +0300

    added struct

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -5,55 +5,42 @@ import (
 	"strings"
 )
 
+type wordFrequency struct {
+	Word  string
+	Count int
+}
+
 func Top10(text string) []string {
 	tempStr := strings.Fields(text)
 	m := make(map[string]int)
 	for _, word := range tempStr {
-		if _, ok := m[word]; !ok {
-			m[word] = 1
-		} else {
-			m[word]++
-		}
+		m[word]++
 	}
-	values := SortAndDuplicateValues(m)
-	result := make([]string, 0, len(m))
-	for _, value := range values {
-		var tempRes []string
-		for k, v := range m {
-			if v == value {
-				tempRes = append(tempRes, k)
-			}
-		}
-		sort.Strings(tempRes)
-		result = append(result, tempRes...)
-		if len(result) == 10 {
+
+	resultStruct := SortValues(m)
+	var result []string
+	for i, v := range resultStruct {
+		result = append(result, v.Word)
+		if i == 9 {
 			break
 		}
 	}
 	return result
 }
 
-func SortAndDuplicateValues(m map[string]int) []int {
-	values := make([]int, 0, len(m))
-	for _, v := range m {
-		values = append(values, v)
+func SortValues(m map[string]int) []wordFrequency {
+	valuesStruct := make([]wordFrequency, 0, len(m))
+	for word, count := range m {
+		valuesStruct = append(valuesStruct, wordFrequency{
+			Word:  word,
+			Count: count,
+		})
 	}
-	sort.Slice(values, func(i, j int) bool {
-		return values[i] > values[j]
+	sort.SliceStable(valuesStruct, func(i, j int) bool {
+		return valuesStruct[i].Word < valuesStruct[j].Word
 	})
-	RemoveDuplicates(&values)
-	return values
-}
-
-func RemoveDuplicates(xs *[]int) {
-	found := make(map[int]bool)
-	j := 0
-	for i, x := range *xs {
-		if !found[x] {
-			found[x] = true
-			(*xs)[j] = (*xs)[i]
-			j++
-		}
-	}
-	*xs = (*xs)[:j]
+	sort.SliceStable(valuesStruct, func(i, j int) bool {
+		return valuesStruct[i].Count > valuesStruct[j].Count
+	})
+	return valuesStruct
 }

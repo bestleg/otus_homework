f26c8ef05a1d2c6c5e1cf696f7fab4e441052077
Oleg Losyakov <olosyakov@ozon.ru>
2021-10-18 13:01:59 +0300

    root circle list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "log"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -16,11 +18,93 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
+//// Next returns the next list element or nil.
+//func (e *ListItem) Next() *ListItem {
+//	if p := e.next; e.list != nil && p != &e.list.root {
+//		return p
+//	}
+//	return nil
+//}
+//
+//// Prev returns the previous list element or nil.
+//func (e *ListItem) Prev() *ListItem {
+//	if p := e.prev; e.list != nil && p != &e.list.root {
+//		return p
+//	}
+//	return nil
+//}
+
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	root  ListItem
+	first ListItem
+	last  ListItem
+	len   int
 }
 
-func NewList() List {
-	return new(list)
+func (l *list) Init() *list {
+	l.root.Next = &l.root
+	l.root.Prev = &l.root
+	l.len = 0
+	return l
+}
+
+func NewList() *list { return new(list).Init() }
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.Next
+}
+
+func (l *list) Back() *ListItem {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.Prev
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newFirstItem := &ListItem{
+		Value: v,
+	}
+	newFirstItem.Prev = &l.root
+	newFirstItem.Next = l.root.Next
+	newFirstItem.Prev.Next = newFirstItem
+	newFirstItem.Next.Prev = newFirstItem
+	l.len++
+	log.Println("front", newFirstItem)
+	return newFirstItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newLastItem := &ListItem{
+		Value: v,
+	}
+	newLastItem.Prev = l.root.Prev
+	newLastItem.Next = &l.root
+	newLastItem.Prev.Next = newLastItem
+	newLastItem.Next.Prev = newLastItem
+	l.len++
+	log.Println("back", newLastItem)
+	return newLastItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	i.Next = nil // avoid memory leaks
+	i.Prev = nil // avoid memory leaks
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	i.Next = l.root.Next
+	l.root.Next = i
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -46,6 +46,7 @@ func TestList(t *testing.T) {
 		for i := l.Front(); i != nil; i = i.Next {
 			elems = append(elems, i.Value.(int))
 		}
+
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
 }

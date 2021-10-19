76e9627dd2f0340328f5a11f24603290f5e2dedf
Oleg Losyakov <olosyakov@ozon.ru>
2021-10-19 20:47:26 +0300

    list works

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,7 +1,5 @@
 package hw04lrucache
 
-import "log"
-
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -13,98 +11,91 @@ type List interface {
 }
 
 type ListItem struct {
-	Value interface{}
-	Next  *ListItem
-	Prev  *ListItem
+	Value      interface{}
+	Next, Prev *ListItem
 }
-
-//// Next returns the next list element or nil.
-//func (e *ListItem) Next() *ListItem {
-//	if p := e.next; e.list != nil && p != &e.list.root {
-//		return p
-//	}
-//	return nil
-//}
-//
-//// Prev returns the previous list element or nil.
-//func (e *ListItem) Prev() *ListItem {
-//	if p := e.prev; e.list != nil && p != &e.list.root {
-//		return p
-//	}
-//	return nil
-//}
 
 type list struct {
-	root  ListItem
-	first ListItem
-	last  ListItem
-	len   int
+	front, back *ListItem
+	len         int
 }
 
-func (l *list) Init() *list {
-	l.root.Next = &l.root
-	l.root.Prev = &l.root
-	l.len = 0
-	return l
+func NewList() List {
+	return &list{}
 }
 
-func NewList() *list { return new(list).Init() }
-
-func (l *list) Len() int {
+func (l list) Len() int {
 	return l.len
 }
 
-func (l *list) Front() *ListItem {
-	if l.len == 0 {
-		return nil
-	}
-	return l.root.Next
+func (l list) Front() *ListItem {
+	return l.front
 }
 
-func (l *list) Back() *ListItem {
-	if l.len == 0 {
-		return nil
-	}
-	return l.root.Prev
+func (l list) Back() *ListItem {
+	return l.back
 }
 
 func (l *list) PushFront(v interface{}) *ListItem {
-	newFirstItem := &ListItem{
-		Value: v,
+	newFirstItem := &ListItem{Value: v}
+
+	if l.back == nil {
+		l.back = newFirstItem
+	} else {
+		newFirstItem.Next = l.front
+		l.front.Prev = newFirstItem
 	}
-	newFirstItem.Prev = &l.root
-	newFirstItem.Next = l.root.Next
-	newFirstItem.Prev.Next = newFirstItem
-	newFirstItem.Next.Prev = newFirstItem
+
+	l.front = newFirstItem
 	l.len++
-	log.Println("front", newFirstItem)
+
 	return newFirstItem
 }
 
 func (l *list) PushBack(v interface{}) *ListItem {
-	newLastItem := &ListItem{
-		Value: v,
+	i := &ListItem{Value: v}
+
+	if l.front == nil {
+		l.front = i
+	} else {
+		i.Prev = l.back
+		l.back.Next = i
 	}
-	newLastItem.Prev = l.root.Prev
-	newLastItem.Next = &l.root
-	newLastItem.Prev.Next = newLastItem
-	newLastItem.Next.Prev = newLastItem
+
+	l.back = i
 	l.len++
-	log.Println("back", newLastItem)
-	return newLastItem
+
+	return i
 }
 
 func (l *list) Remove(i *ListItem) {
-	i.Prev.Next = i.Next
-	i.Next.Prev = i.Prev
-	i.Next = nil // avoid memory leaks
-	i.Prev = nil // avoid memory leaks
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.front = i.Prev
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.back = i.Next
+	}
+
 	l.len--
 }
 
 func (l *list) MoveToFront(i *ListItem) {
-	i.Prev.Next = i.Next
-	i.Next.Prev = i.Prev
-	i.Next = l.root.Next
-	l.root.Next = i
+	// l.Remove(i)
+	// l.PushFront(i.Value)
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		return
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	i.Prev = nil
+	i.Next = l.front
+	l.front = i
 }

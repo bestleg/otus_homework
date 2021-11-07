c655d044b7af998cdc8beb627f18594a3b2974c7
Oleg Losyakov <olosyakov@ozon.ru>
2021-11-07 18:43:30 +0300

    hw06 initial commit

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,33 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = stage(doneStage(out, done))
+	}
+	return doneStage(out, done)
+}
+
+func doneStage(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer func() {
+			close(out)
+			for range in {
+			}
+		}()
+
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
 }
